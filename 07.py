6c462ecdece800db112442c48982b2db1c017a6c
Thuani Silva <thuanisc@gmail.com>
2025-05-05 20:03:19 -0300

    Questão 5 - if else input ternário

--- 07.py
+++ 07.py
@@ -1,7 +1,12 @@
 #7. Declare uma variável booleana que verifica se um número (entrada como inteiro) é par, depois imprima o número e o resultado booleano.
 
 numero = int(input("Digite um número inteiro: "))
-eh_par = (numero % 2 == 0)
+eh_par = (numero % 2 == 0) #já vai direto
 
+print(f"O número digitado foi: {numero}")
+print(f"O número é par? {eh_par}")
+
+#opção 2 - usando ternário
+eh_par = Truf if numero%2==0 else False
 print(f"O número digitado foi: {numero}")
 print(f"O número é par? {eh_par}")
