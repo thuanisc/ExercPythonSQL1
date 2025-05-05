04e03f3817ec29923115557332936ba195674327
thuanisc <thuanisc@gmail.com>
2025-05-05 15:43:18 -0300

    Questão 02

---
+++ 02.py
@@ -0,0 +1,15 @@
+#2. Crie um programa que receba dois números como entrada e imprima sua soma, diferença, produto e divisão.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+diferenca = num1 - num2
+produto = num1 * num2
+divisao = num1 / num2
+
+# Imprime os resultados
+print(f"Soma: {soma}")
+print(f"Diferença: {diferenca}")
+print(f"Produto: {produto}")
+print(f"Divisão: {divisao}")
