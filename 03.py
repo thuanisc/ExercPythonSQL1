9ea61eb0ed332c58eb97b6941a4e24a2dd0f286a
thuanisc <thuanisc@gmail.com>
2025-05-05 15:45:37 -0300

    Questão 02

---
+++ 03.py
@@ -0,0 +1,4 @@
+#3. Peça ao usuário sua cor favorita e imprima em uma frase, como "Sua cor favorita é [cor]!"
+
+cor = input("Qual é a sua cor favorita? ")
+print(f"Sua cor favorita é {cor}, que bacana!")
