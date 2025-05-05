3159df2230f40f553673e20824dd7776c317025c
Thuani Silva <thuanisc@gmail.com>
2025-05-05 20:15:41 -0300

    Questão 8 - mais opçoes

--- 08.py
+++ 08.py
@@ -5,3 +5,14 @@ tem_mais_de_5 = len(texto)>5
 
 print(f"Você digitou: {texto}")
 print(f"A string tem mais de 5 caracteres? {tem_mais_de_5}")
+
+#opção 2 - se quiser especificar mais
+tamanho_texto = len(texto)
+tem_mais_de_5 = None
+
+if tamanho_texto > 5:
+    tem_mais_de_5 = True
+else:
+    tem_mais_de_5 = False
+
+print("O texto é", texto, " e o boolean é", tem_mais_de_5)
