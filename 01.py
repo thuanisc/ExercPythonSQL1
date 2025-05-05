528576f74aa9fa55ea326ec049138841fd29f14d
thuanisc <thuanisc@gmail.com>
2025-05-05 15:35:24 -0300

    Questão 01

---
+++ 01.py
@@ -0,0 +1,6 @@
+#1. Escreva um programa que peça o nome e a idade do usuário e imprima uma mensagem de saudação como "Olá, [nome]! Você tem [idade] anos."
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
